bb025131809015c3c437c1dbde76fe4ccf2419d3
RAI <ada.rizeanu@gmail.com>
2025-03-13 23:36:46 +0200

    Update

---
+++ Seminar3/src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import ro.cts.autoritateMasini.clase.AutoritateReglementareMasiniEager;
+
+public class Main {
+        public static void main(String[] args) {
+            AutoritateReglementareMasiniEager autoritate
+                    = AutoritateReglementareMasiniEager.getInstance();
+            System.out.println(autoritate);
+
+            AutoritateReglementareMasiniEager autoritate2
+                    = AutoritateReglementareMasiniEager.getInstance();
+            autoritate2.setWebSite("www.siteNou_autoritate2.ro");
+            System.out.println(autoritate2);
+
+            System.out.println(autoritate);
+
+            autoritate2.reglementeazaModelMasina("Dacia Logan");
+            System.out.println(autoritate2);
+        }
+
+    }
